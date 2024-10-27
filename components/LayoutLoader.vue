f1184cd538acad5ae8e99d946c50ddffcf384ba6
aadiityaak <aadiityaak@gmail.com>
2024-10-27 21:51:25 +0700

    tambah layoutloader

---
+++ components/LayoutLoader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="layout-loader">
+      <p>Loading...</p>
+    </div>
+  </template>
--- plugins/lucide.js
+++ plugins/lucide.js
@@ -1,6 +1,6 @@
-import { defineNuxtPlugin } from '#app'
-import { Lucide } from 'lucide-vue-next'
+import { defineNuxtPlugin } from "#app";
+import { Lucide } from "lucide-vue-next";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Lucide)
-})
+  nuxtApp.vueApp.use(Lucide);
+});
